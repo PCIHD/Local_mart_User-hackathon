7a005b5eb07ccf2c49db57f991868ec9f8fc98c1
parag <Parag.chaudharis@gmail.com>
2020-07-21 19:15:44 +0530

    added functionality provider

---
+++ src/context/FunctionalityContext.js
@@ -0,0 +1,24 @@
+import createDataContext from './createDataContext';
+import {Auth} from 'aws-amplify';
+import AsyncStorage from '@react-native-community/async-storage';
+import {navigate} from '../navigationRef';
+
+const functionalityReducer = (state, action) => {
+  switch (action.type) {
+    //add state change actions here
+
+    default:
+      return state;
+  }
+};
+
+const get_location = dispatch => {
+  return async ({username, code}) => {
+    //put get location code here
+  };
+};
+export const {Provider, Context} = createDataContext(
+  functionalityReducer,
+  {}, // add methods to expose to other screens
+  {}, // add states to expose to  other screens
+);
